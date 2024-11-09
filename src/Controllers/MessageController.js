265d316273af076cd4b9f7e78a9b7ef2e92b6524
AlfonsoG-dev <alfgomachi@gmail.com>
2024-11-08 22:54:39 -0500

    feat: implementation of message controller

---
+++ src/Controllers/MessageController.js
@@ -0,0 +1,67 @@
+import mysql2 from "mysql2"
+import assert from "assert"
+
+import MessageRepository from "../Repositories/MessageRepository.js"
+
+export default class MessageController {
+    message_repo
+    constructor(cursor = mysql2.createConnection("")) {
+        this.message_repo = new MessageRepository(cursor)
+    }
+    async get_all(req, res) {
+        try {
+            const data_req = req.params
+            assert.notEqual(Object.keys(data_req).length, 0)
+            const data_res = await this.message_repo.select(data_req)
+            if(data_res.length === 0) {
+                return res.json({error: "Empty set"})
+            }
+            return res.status(200).json(data_res)
+        } catch(er) {
+            console.error(er)
+            return res.status(400).json({error: "[Error]: while trying to get all messages"})
+        }
+    }
+    async get_message_by_id(req, res) {
+        try {
+            const data_req = req.params
+            assert.notEqual(Object.keys(data_req).length, 0)
+            const data_res = await this.message_repo.select(data_req)
+            if(data_res.length === 0) {
+                return res.json({error: "Not found"})
+            }
+            return res.status(200).json(data_res)
+        } catch(er) {
+            console.error(er)
+            return res.status(400).json({error: "[Error]: while trying to get message by id_pk"})
+        }
+    }
+    async get_messages_by_user(req, res) {
+        try {
+            const data_req = req.params
+            assert.notEqual(Object.keys(data_req).length, 0)
+            const data_res = this.message_repo.select(data_req)
+            if(data_res.length === 0) {
+                return res.json({error: "Not found"})
+            }
+            return res.status(200).json(data_res)
+        } catch(er) {
+            console.error(er)
+            return res.status(400).json({error: "[Error]: while trying to get messages by user"})
+        }
+    }
+    async post_message(req, res) {
+        try {
+            const data_req = req.body
+            assert.notEqual(data_req, undefined)
+            const data_res = await this.message_repo.insert(data_req)
+            if(data_res.affectedRows === 0) {
+                return res.status(409).json({error: "Cannot insert message"})
+            }
+            return res.status(200).json({msg: "Message has been inserted"})
+        } catch(er) {
+            console.error(er)
+            return res.status(400).json({error: "[Error]: while trying to insert message"})
+        }
+    }
+}
