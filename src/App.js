56c975c68d46102481be375d8a000ad0d67387cd
AlfonsoG-dev <alfgomachi@gmail.com>
2024-11-08 22:58:10 -0500

    refactor: complete message routes with controller & use the router in the server

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ configDotenv()
 import DBConfig from "./DataBase/DBConfig.js"
 
 import UserRoute from "./Routes/UserRoute.js"
+import MessageRoute from "./Routes/MessageRoute.js"
 
 const PORT = process.env.API_PORT
 const HOST = process.env.API_HOST
@@ -16,11 +17,12 @@ app.get("/", (req, res) => {
     res.status(200).json({
         msg: "Hellow motherr",
         ips: req.ip,
-        valid: cursor.authorized
+        valid_db_connection: cursor.authorized
     })
 })
 
 app.use("/user", new UserRoute(cursor).routes)
+app.use("/message", new MessageRoute(cursor).routes)
 
 
 app.listen(PORT, HOST, () => {
--- src/Routes/MessageRoute.js
+++ src/Routes/MessageRoute.js
@@ -1,28 +1,44 @@
 import mysql2 from "mysql2"
 import { Router } from "express"
 
+import MessageController from "../Controllers/MessageController.js"
+
 export default class MessageRoute {
     router
+    message_controller
     constructor(cursor = mysql2.createConnection("")) {
         this.router = Router()
+        this.message_controller = new MessageController(cursor)
     }
 
     get_messages() {
-        this.router.get("/all/:limit/:offset")
+        this.router.get("/all/:limit/:offset", 
+            this.message_controller.get_all.bind(this.message_controller)
+        )
     }
 
     get_message_by_id() {
-        this.router.get("/by-id/:id_pk")
+        this.router.get("/by-id/:id_pk", 
+            this.message_controller.get_message_by_id.bind(this.message_controller)
+        )
     }
     get_messages_by_user() {
-        this.router.get("/by-user/:user_id_fk")
+        this.router.get("/by-user/:user_id_fk",
+            this.message_controller.get_messages_by_user.bind(this.message_controller)
+        )
     }
 
     post_message() {
-        this.router.post("/post-message")
+        this.router.post("/post-message", 
+            this.message_controller.post_message(this.message_controller)
+        )
     }
 
     get routes() {
+        this.get_messages()
+        this.get_message_by_id()
+        this.get_messages_by_user()
+        this.post_message()
         return this.router
     }
 }
